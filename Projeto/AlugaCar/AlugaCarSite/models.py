c0e03851776d8a69806525031264627ed194082a
pedrobertella <pe_pedro@outlook.com>
2019-07-04 20:26:28 -0300

    Models e outras coisas mais

--- Projeto/AlugaCar/AlugaCarSite/models.py
+++ Projeto/AlugaCar/AlugaCarSite/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Carro(models.Model):
-    desricao = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=50)
+    disponivel = models.BooleanField(default=True)
+    valorDia = models.DecimalField(max_digits=10, decimal_places=2)
+    marca = models.CharField(max_length=25)
+    modelo = models.CharField(max_length=25)    
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=50)
@@ -11,3 +15,6 @@ class Cliente(models.Model):
 class Aluguel(models.Model):
     cliente = models.ForeignKey(Cliente,on_delete = models.SET_NULL, null = True)
     carrro = models.ForeignKey(Carro, on_delete=models.CASCADE, null = False)
+    dataAlugado = models.DateField()
+    diasAluguel = models.IntegerField()
+    valorPagar = models.DecimalField(max_digits=10, decimal_places=2)
--- Projeto/AlugaCar/AlugaCarSite/urls.py
+++ Projeto/AlugaCar/AlugaCarSite/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import cadastro_user
 
 urlpatterns = [
-    path("cadastrouser", cadastro_user, name="cadastro_user"),
+    path("cadastrouser/", cadastro_user, name="cadastro_user"),
 ]
