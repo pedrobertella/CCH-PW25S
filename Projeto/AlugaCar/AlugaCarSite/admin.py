25609aba8d2ec2e5e998b25a8f85117a8c443d53
jailsonpanizzon <jailson.panizzon@gmail.com>
2019-07-04 14:24:10 -0300

    Models

--- Projeto/AlugaCar/AlugaCarSite/admin.py
+++ Projeto/AlugaCar/AlugaCarSite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Carro,Cliente,Aluguel
+
+admin.site.register(Carro)
+
+admin.site.register(Cliente)
+
+admin.site.register(Aluguel)
--- Projeto/AlugaCar/AlugaCarSite/models.py
+++ Projeto/AlugaCar/AlugaCarSite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Carro(models.Model):
+    desricao = models.CharField((""), max_length=50)
+
+class Cliente(models.Model):
+    nome = models.CharField((""), max_length=50)
+    email = models.CharField((""), max_length=50)
+    senha = models.CharField((""), max_length=50)
+
+class Aluguel(models.Model):
+    cliente = models.ForeignKey(Cliente,on_delete = models.SET_NULL, null = True)
+    carrro = models.ForeignKey(Carro, on_delete=models.CASCADE, null = False)
