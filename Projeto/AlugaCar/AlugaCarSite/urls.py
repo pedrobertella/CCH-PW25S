deb0deeaeb3b3ce485a71d4ac76829e4a1db6366
pedrobertella <pe_pedro@outlook.com>
2019-07-04 21:36:14 -0300

    Inicio de template

--- Projeto/AlugaCar/AlugaCarSite/urls.py
+++ Projeto/AlugaCar/AlugaCarSite/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import cadastro_user,login_user
+from .views import cadastro_user,login_user, home
 
 urlpatterns = [
+    path("", home, name="home"),
     path("cadastrouser", cadastro_user, name="cadastro_user"),
     path("login", login_user,name="login_user"),
 ]
