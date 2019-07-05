a1954493b750d454d38e8ff79a3a3d187fd3a553
jailsonpanizzon <jailson.panizzon@gmail.com>
2019-07-05 03:40:22 -0300

    changes

--- Projeto/AlugaCar/AlugaCarSite/urls.py
+++ Projeto/AlugaCar/AlugaCarSite/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import cadastro_user,login_user, home
+from .views import cadastro_user,login_user, home,user_page,cancelar_aluguel
 
 urlpatterns = [
     path("", home, name="home"),
     path("cadastrouser", cadastro_user, name="cadastro_user"),
     path("login", login_user,name="login_user"),
+    path("user_page", user_page,name="user_page"),
+    path("delete/<int:id>", cancelar_aluguel,name="cancelar_aluguel")
 ]
--- Projeto/AlugaCar/AlugaCarSite/views.py
+++ Projeto/AlugaCar/AlugaCarSite/views.py
@@ -1,6 +1,7 @@
+
 from django.shortcuts import render,redirect
 from .form import ClienteForm,LoginUserForm
-from .models import Cliente,Carro
+from .models import Cliente,Carro,Aluguel
 
 def home(request):
     id = request.session.get('login', None)
@@ -19,6 +20,7 @@ def cadastro_user(request):
         return redirect("home")
     return render(request, 'cadastro_user.html', {'form':form})
 
+
 def login_user(request):
     form = LoginUserForm(request.POST or None)
     if(form.is_valid()):
@@ -33,3 +35,17 @@ def login_user(request):
                     request.session['login'] = i.id
                     return redirect('home')
     return render(request,'login.html',{'form':form})
+
+def user_page(request):
+    user = Cliente.objects.get(id=request.session.get('login'))
+    alu = Aluguel.objects.all()
+    alugueis=[]
+    for i in alu:
+        if i.cliente.id == user.id:
+            alugueis.append(i)
+
+    return render(request,"user_page.html",{"cliente":user,'alugueis':alugueis})
+
+def cancelar_aluguel(request,id):
+    Aluguel.objects.get(id=id).delete()
+    return redirect("user_page")
