d2600f644a580d554e9847c044e08d2e4627a3ea
pedrobertella <pe_pedro@outlook.com>
2019-07-05 16:59:11 -0300

    Coisas

--- Projeto/AlugaCar/AlugaCarSite/urls.py
+++ Projeto/AlugaCar/AlugaCarSite/urls.py
@@ -4,5 +4,5 @@ from .views import cadastro_user,login_user, home
 urlpatterns = [
     path("", home, name="home"),
     path("cadastrouser", cadastro_user, name="cadastro_user"),
-    path("login", login_user,name="login_user"),
+    path("login", login_user, name="login_user"),
 ]
